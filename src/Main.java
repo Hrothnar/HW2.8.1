69a90feaa47b18ada92b86f8256200d520b6ed70
Kirill <hrothnar@mail.ru>
2022-11-12 20:11:56 +0600

    homework was done

--- src/Main.java
+++ src/Main.java
@@ -36,18 +36,5 @@ public class Main {
         Recipe.addRecipeToCookbook(fruitSalad1);
 
 
-        Set<Integer> list = new HashSet<>();
-        for (int i = 0; list.size() < 20; i++) {
-            int num = (int) (Math.random() * 1000);
-            list.add(num);
-        }
-        Iterator<Integer> iter = list.iterator();
-        while (iter.hasNext()) {
-            if (iter.next() % 2 != 0)
-                iter.remove();
-        }
-        System.out.println(Arrays.toString(list.toArray()) + "   Размер: " + list.size());
-
-
     }
 }
